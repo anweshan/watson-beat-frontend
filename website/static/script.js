2b05d3d4bada17ab6bb0ab99187e3de1489308c9
Dan Friedman <dan.friedman1@ibm.com>
2018-05-07 16:29:19 -0400

    Update formatting

--- website/static/script.js
+++ website/static/script.js
@@ -8,17 +8,19 @@ COMPLEXITIES = ["simple", "super_simple", "semi_complex"];
 Vue.component('text-input', {
   props: ['name', 'id', 'help', 'value'],
   template: `
-<div class="form-group">
-  <label :for="id">{{name}}</label>
+<div class="form-group mx-1">
+  <label :for="id">
+    {{name}}
+    <a href="#" :title="help">
+      ?
+    </a>
+  </label>
   <input type="text" class="form-control"
          :aria-described-by="id + '-help'"
          :id="id"
          :name="id"
          :value="value">
   </input>
-  <small :id="id + '-help'" class="form-text text-muted" v-if="help">
-    {{ help }}
-  </small>
 </div>
 `
 });
@@ -28,16 +30,18 @@ Vue.component('text-input', {
 Vue.component('select-input', {
   props: ['name', 'id', 'help', 'options'],
   template: `
-<div class="form-group">
-  <label :for="id">{{ name }}</label>
+<div class="form-group mx-1">
+  <label :for="id">
+    {{name}}
+    <a href="#" :title="help">
+      ?
+    </a>
+  </label>
   <select :id="id" class="form-control"
           :aria-described-by="id+ '-help'"
           :name="id">
     <option v-for="opt in options">{{ opt }}</option>
   </select>
-  <small :id="name + '-help'" class="form-text text-muted">
-    {{ help }}
-  </small>
 </div>
 `
 });
@@ -48,49 +52,51 @@ Vue.component('wb-section', {
   props: ['movementId', 'sectionId'],
   template: `
 <section>
-  <h5>Section {{ sectionId }}</h5>
-  <text-input
-    name="Time signature"
-    :id="getId('tse')"
-    help="Time signature, e.g. '4/4'"
-    value="4/4">
-  </text-input>
-  <select-input
-    name="Energy"
-    :id="getId('energy')"
-    help="The number of layers in a range playing during a section (low, medium, high). Each of these categories represents a range of layers that can be playing. For instance, 'low' can be 1-4 layers while 'high' can be 3-12 layers."
-    :options="['low', 'medium', 'high']">
-  </select-input>
-  <text-input
-    name="Beats per minute"
-    :id="getId('bpm')"
-    help="Tempo in beats per minute (e.g. '100')"
-    value="100">
-  </text-input>
-  <text-input
-    name="Duration"
-    :id="getId('duration')"
-    help="Min and max number of seconds a section can be (e.g. '10 to 20 seconds')"
-    value="10 to 20 seconds">
-  </text-input>
-  <text-input
-    name="Duration in measures"
-    :id="getId('durationInMeasures')"
-    help="Number of measures a section can be. Can be one or two options to choose between (e.g. '4', '2 or 4')"
-    value="4">
-  </text-input>
-  <select-input
-    name="Slope"
-    :id="getId('slope')"
-    help="The rate of change in the density level of a section"
-    :options="['stay', 'gradual', 'steep']">
-  </select-input>
-  <select-input
-    name="Direction"
-    :id="getId('direction')"
-    help="Determines whether layers will be added or removed during a section ('up' - adds layers, 'down' - removes layers)"
-    :options="['up', 'down']">
-  </select-input>
+  <h5>section {{ sectionId }}</h5>
+  <div class="d-flex">
+    <text-input
+      name="Time signature"
+      :id="getId('tse')"
+      help="Time signature, e.g. '4/4'"
+      value="4/4">
+    </text-input>
+    <select-input
+      name="Energy"
+      :id="getId('energy')"
+      help="The number of layers in a range playing during a section (low, medium, high). Each of these categories represents a range of layers that can be playing. For instance, 'low' can be 1-4 layers while 'high' can be 3-12 layers."
+      :options="['low', 'medium', 'high']">
+    </select-input>
+    <text-input
+      name="Beats per minute"
+      :id="getId('bpm')"
+      help="Tempo in beats per minute (e.g. '100')"
+      value="100">
+    </text-input>
+    <text-input
+      name="Duration"
+      :id="getId('duration')"
+      help="Min and max number of seconds a section can be (e.g. '10 to 20 seconds')"
+      value="10 to 20 seconds">
+    </text-input>
+    <text-input
+      name="Duration in measures"
+      :id="getId('durationInMeasures')"
+      help="Number of measures a section can be. Can be one or two options to choose between (e.g. '4', '2 or 4')"
+      value="4">
+    </text-input>
+    <select-input
+      name="Slope"
+      :id="getId('slope')"
+      help="The rate of change in the density level of a section"
+      :options="['stay', 'gradual', 'steep']">
+    </select-input>
+    <select-input
+      name="Direction"
+      :id="getId('direction')"
+      help="Determines whether layers will be added or removed during a section ('up' - adds layers, 'down' - removes layers)"
+      :options="['up', 'down']">
+    </select-input>
+  </div>
 </section>
 `,
   methods: {
@@ -113,34 +119,35 @@ Vue.component('movement', {
   },
   template: `
 <section>
-  <h3>Movement {{ movementId }}</h3>
-  <text-input
-    name="movementDuration"
-    :id="getId('movementDuration')"
-    help="Duration of the movement in seconds (e.g. 60)"
-    value="60">
-  </text-input>
-  <select-input
-    name="mood"
-    :id="getId('mood')"
-    help="'mood' for the movement"
-    :options="getMoods()">
-  </select-input>
-  <select-input
-    name="complexity"
-    :id="getId('complexity')"
-    help="Determines the complexity of the chord progressions"
-    :options="getComplexities()">
-  </select-input>
-  <section>
-    <h4>Sections:</h4>
+  <h3>movement {{ movementId }}</h3>
+  <div class="d-flex">
+    <text-input
+      name="movementDuration"
+      :id="getId('movementDuration')"
+      help="Duration of the movement in seconds (e.g. 60)"
+      value="60">
+    </text-input>
+    <select-input
+      name="mood"
+      :id="getId('mood')"
+      help="'mood' for the movement"
+      :options="getMoods()">
+    </select-input>
+    <select-input
+      name="complexity"
+      :id="getId('complexity')"
+      help="Determines the complexity of the chord progressions"
+      :options="getComplexities()">
+    </select-input>
+  </div>
+  <section class="border p-2 m-2">
     <wb-section
       v-for="i in numSections"
       :key="i - 1"
       :movementId="movementId"
       :sectionId="i - 1">
     </wb-section>
-    <p><button @click.stop.prevent="numSections += 1">Add a section</button></p>
+    <button @click.stop.prevent="numSections += 1">Add a section</button>
   </section>
 </section> 
 `,
@@ -165,15 +172,14 @@ Vue.component('composition', {
     };
   },
   template: `
-<div>
-  <h2>Movements:</h2>
+<section class="border p-2 mb-2">
   <movement
     v-for="i in numMovements"
     :key="i - 1"
     :movementId="i - 1">
   </movement>
-  <p><button @click.stop.prevent="numMovements += 1">Add a movement</button></p>
-</div>
+  <button @click.stop.prevent="numMovements += 1">Add a movement</button>
+</section>
 `
 });
 
